439dcf3e12c7210020a45dba10ecde99b341a04f
Pooja3313 <21cp324@bvmengineering.ac.in>
2023-08-05 13:09:18 +0530

    f44

--- client/src/Components/Login/Login.js
+++ client/src/Components/Login/Login.js
@@ -18,7 +18,7 @@ function Login(){
         <form>
           <div className="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
             <p className="lead fw-normal mb-0 me-3">Sign in with</p>
-            <button type="button" className="btn btn- btn-floating mx-1">
+            <button type="button" className="btn btn-primary btn-floating mx-1">
             <NavLink to="#!" className="Google"><FontAwesomeIcon icon={faGoogle} />
             </NavLink>
             </button>
